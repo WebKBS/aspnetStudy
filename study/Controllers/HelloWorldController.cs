5658e63dc27a2c88cf68bd37702e79f7c9aa442a
gangbeomsu <kbs21c1987@gmail.com>
2023-06-20 22:21:00 +0900

    컨트롤러 시용빙법 새그먼트와 패스 아이디

---
+++ study/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace study.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int number = 2)
+    {
+        //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        return HtmlEncoder.Default.Encode($"Hello {name}, Number = {number}");
+    }
+}
+
