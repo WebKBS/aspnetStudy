ea2fa8950d070db201a4f48b8931a0ecfd31b8f9
gangbeomsu <kbs21c1987@gmail.com>
2023-06-20 23:17:39 +0900

    모델 준비

---
+++ study/Models/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace study.Models;
+
+	public class Hello
+	{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+
+    [DataType(DataType.Date)]
+
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
+
+
+
